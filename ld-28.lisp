d8a104ab0c0862baa1d90e9e8f01986347382d56
Benjamin Meinl <b.meinl@gmail.com>
2013-12-19 03:34:24 +0100

    Classes set up.

--- ld-28.lisp
+++ ld-28.lisp
@@ -9,19 +9,37 @@
 ;;; =======
 
 (defclass menu-screen ()
-  ((options :accessor menu-items
-            :initarg :options
-            :initform '("Play"
-                        "Do nothing"
-                        "Quit"))
-   (selection :accessor menu-selection
-              :initarg :selection
-              :initform 0)))
+  ((options :accessor options
+            :initarg :options)
+   (selection :accessor selection
+              :initarg :selection))
+  (:default-initargs
+   :options '("Play" "Quit")
+   :selection 0))
 
 (defclass game-screen ()
-  ((level :accessor game-level
-          :initarg :level
-          :initform 0)))
+  ((player :accessor player
+           :initarg :player))
+  (:default-initargs
+   :player (make-instance 'entity
+                          :shape '((-25 . -25)
+                                   (25 . -25)
+                                   (25 . 25)
+                                   (-25 . 25))
+                          :auto-vectorize t)))
+
+(defclass entity ()
+  ((shape :accessor shape
+          :initarg :shape)
+   (transform :accessor transform
+              :initarg :transform))
+  (:default-initargs
+   :transform (lm:make-identity 3)
+   :auto-vectorize nil))
+
+(defmethod initialize-instance :after ((e entity) &key shape auto-vectorize)
+  (when auto-vectorize
+    (setf (shape e) (mapcar 'cons->vector3 shape))))
 
 ;;; UPDATE METHODS
 ;;; ==============
@@ -50,19 +68,7 @@
 
 (defmethod draw ((screen game-screen))
   (sdl:clear-display sdl:*black*)
-  (sdl:with-color (col (sdl:color :r 100 :g 100 :b 100))
-    (loop :for i :from 0
-       :for s :in '("Press Right to win the level!"
-                    "Left to go back to previously beat levels."
-                    "Escape to go back to menu.")
-       :do (sdl:draw-string-solid-* s 10 (* 10 (1+ i)))))
-  
-  (sdl:with-font (font (make-instance 'SDL:ttf-font-definition
-                                      :size 72
-                                      :filename sdl:*default-ttf-font*))
-    (sdl:draw-string-solid-* (format nil "Level ~A" (game-level screen))
-                             10
-                             (- (/ (aref (sdl:video-dimensions) 1) 2) 36))))
+  )
 
 ;;; HANDLE KEY METHODS
 ;;; ==================
@@ -82,7 +88,7 @@
 
 (defmethod handle-key ((screen game-screen) key)
   (case key
-    (:sdl-key-escape (setf *screen* (make-instance 'menu-screen)))
+    (:sdl-key-escape (sdl:push-quit-event))
     (:sdl-key-left (setf (game-level screen) (max 0 (1- (game-level screen)))))
     (:sdl-key-right (incf (game-level screen)))))
 
@@ -90,7 +96,7 @@
 ;;; =========
 
 (defun run (&key (width 460) (height 284)) ; golden ratio yo
-  (setf *screen* (make-instance 'menu-screen))
+  (setf *screen* (make-instance 'game-screen))
   (sdl:with-init ()
     (sdl:window width height :title-caption "You Only Drop Once")
     (setf (sdl:frame-rate) 60)
@@ -102,3 +108,9 @@
         (update *screen*)     
         (draw *screen*)
         (sdl:update-display)))))
+
+;;; MISC HELPER FUNCTIONS
+;;; =====================
+
+(defun cons->vector3 (cons)
+  (lm:vector (car cons) (cdr cons) 0))
